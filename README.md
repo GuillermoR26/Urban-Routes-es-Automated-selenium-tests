a9c0e9834b96b5878353f20b4258e8cc49a3b641
GuillermoR26 <renteriabravojgg102@gmail.com>
2024-08-22 17:50:10 -0700

    1st send project sprint 8

---
+++ README.md
@@ -0,0 +1,21 @@
+# PROYECTO APLICACION URBAN ROUTES
+## Titulo : Pide un taxi 
+
+En este proyecto usamos la plataforma PyCharm, en hojas de Idioma Python, tomando el estudio de la aplicacion mediante 
+DevTools en navegador Google Chrome, realizando pruebas con las herramientas de Pytest de la plataforma. 
+
+EL proyecto se conforma de dos hojas de informacion: data.py y main.py. En data.py podremos encontrar la informacion 
+refente al llenado de la aplicacion. En main.py se encuentra los selectores y las pruebas de la aplicacion.
+
+Un total de 8 pruebas para correr la aplicacion:
+test_1_set_route  
+test_2_comfort_button  
+test_3_phone_number  
+test_4_card_method_payment  
+test_5_placed_comment_to_driver  
+test_6_select_blankets_and_handkerchiefs  
+test_7_select_ice_cream  
+test_8_wait_for_call_a_taxi_active_button  
+
+Cada prueba cuanta con su titulo respecto a lo que se estara haciendo, previo a correr la pruieba deberas tener
+descargado Chrome.WebDriver primero, despues colocar el url en data.py por el actual de la aplicacion, correr tests.
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import data
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 
 
 # no modificar
@@ -37,7 +37,30 @@ def retrieve_phone_code(driver) -> str:
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    request_taxi_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[1]/div[3]/div[1]/button')
+    comfort_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]')
+    comfort_perks = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[1]')
+    phone_number_button = (By.CLASS_NAME, "np-button")
+    phone_number = (By.ID, 'phone')
+    next_button = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button')
+    code_phone_number_input = (By.ID, 'code')
+    confirm_phone_number = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/form/div[2]/button[1]')
+    card = (By.CLASS_NAME, 'pp-button.filled')
+    add_card = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]/div[2]/div[3]')
+    digits_card = (By.XPATH, '//*[@id="number"]')
+    digits_code_card = (By.NAME, 'code')
+    closed_card_method_button = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]/button')
+    add_card_button = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/form/div[3]/button[1]')
+    select_outside_the_CVV_code = (By.CLASS_NAME, 'section.active.unusual')
+    payment_method = (By.CLASS_NAME, 'pp-value-text')
+    comment_to_driver = (By.ID, 'comment')
+    comment_to_driver_button = (By.CLASS_NAME, 'input-container')
+    request_blankets_and_tissues_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[1]')
+    blankets_and_handkerchiefs_label = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]')
+    ice_cream_button_selection = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
+    call_a_taxi_load_page = (By.CLASS_NAME, 'smart-button-wrapper')
 
+    # Para colocar destino inicial y final en la app
     def __init__(self, driver):
         self.driver = driver
 
@@ -53,10 +76,128 @@ class UrbanRoutesPage:
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def set_route(self, from_address, to_address):
+        self.set_from(from_address)
+        self.set_to(to_address)
 
+    # Para seleccionar comfort taxi
+
+    def click_request_taxi_button(self):
+        self.driver.find_element(*self.request_taxi_button).click()
+
+    def click_select_comfort_button(self):
+        self.driver.find_element(*self.comfort_button).click()
+
+    def validated_comfort_choice_with_perks(self):
+        return self.driver.find_element(*self.comfort_perks).text
+
+    def set_comfort_option(self):
+        self.click_request_taxi_button()
+        self.click_select_comfort_button()
+
+    # Para colocar telefono en la app
+    def click_phone_number_button(self):
+        self.driver.find_element(*self.phone_number_button).click()
+        time.sleep(2)
+    def placed_phone_number(self):
+        self.driver.find_element(*self.phone_number).send_keys(data.phone_number)
+        time.sleep(2)
+
+    def click_next_button(self):
+        self.driver.find_element(*self.next_button).click()
+        time.sleep(2)
+
+    def placed_phone_code(self):
+        self.driver.find_element(*self.code_phone_number_input).send_keys(retrieve_phone_code(self.driver))
+        time.sleep(2)
+
+    def click_confirm_phone_number(self):
+        self.driver.find_element(*self.confirm_phone_number).click()
+        time.sleep(2)
+
+    def validated_phone_number(self):
+        return self.driver.find_element(*self.phone_number_button).text
+
+    def set_phone_number(self):
+        self.click_phone_number_button()
+        self.placed_phone_number()
+        self.click_next_button()
+        self.placed_phone_code()
+        self.click_confirm_phone_number()
+
+    # Para colocar tarjeta de pago en la app
+
+    def click_card_button(self):
+        self.driver.find_element(*self.card).click()
+
+    def click_add_card(self):
+        self.driver.find_element(*self.add_card).click()
+        time.sleep(2)
+
+    def placed_digits_card(self):
+        self.driver.find_element(*self.digits_card).send_keys(data.card_number)
+        time.sleep(2)
+
+    def placed_digits_code_card(self):
+        self.driver.find_element(*self.digits_code_card).send_keys(data.card_code)
+        time.sleep(2)
+    def click_add_card_button(self):
+        self.driver.find_element(*self.select_outside_the_CVV_code).click()
+        self.driver.find_element(*self.add_card_button).click()
+        time.sleep(3)
+
+    def click_closed_card_method_button(self):
+        self.driver.find_element(*self.closed_card_method_button).click()
+
+    def validated_payment_method(self):
+        return self.driver.find_element(*self.payment_method).text
+
+    def set_card_method_payment(self):
+        self.click_card_button()
+        self.click_add_card()
+        self.placed_digits_card()
+        self.placed_digits_code_card()
+        self.click_add_card_button()
+        self.click_closed_card_method_button()
+
+    # Para colocar comentario a conductor en la app
+    def placed_comment_to_driver(self):
+        self.driver.find_element(*self.comment_to_driver).send_keys(data.message_for_driver)
+
+    def validated_comment_to_driver(self):
+        return self.driver.find_element(*self.comment_to_driver_button).text
+
+    def set_comment_to_driver(self):
+        self.placed_comment_to_driver()
+
+    # Solicitar mantas y panuelos en la app
+    def wait_for_load_label_blankets_and_handkerchiefs(self):
+        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(self.blankets_and_handkerchiefs_label))
+
+    def click_request_blankets_and_handkerchiefs_button(self):
+        self.driver.find_element(*self.blankets_and_handkerchiefs_label).click()
+    def set_request_blankets_and_handkerchiefs(self):
+        self.wait_for_load_label_blankets_and_handkerchiefs()
+        self.click_request_blankets_and_handkerchiefs_button()
+
+    # Solicitar nieve en la app
+    def wait_for_load_label_ice_cream(self):
+        WebDriverWait(self.driver,3).until(expected_conditions.visibility_of_element_located(self.ice_cream_button_selection))
+
+    def click_ice_cream_button_twice(self):
+        self.driver.find_element(*self.ice_cream_button_selection).click()
+        self.driver.find_element(*self.ice_cream_button_selection).click()
+
+    def set_request_ice_cream(self):
+        self.wait_for_load_label_ice_cream()
+        self.click_ice_cream_button_twice()
+
+    # Para confirmar que aparece boton de "Call a taxi"
+
+    def call_a_taxi_button_visibility(self):
+        return self.driver.find_element(*self.call_a_taxi_load_page).text
 
 class TestUrbanRoutes:
-
     driver = None
 
     @classmethod
@@ -65,10 +206,11 @@ class TestUrbanRoutes:
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        cls.driver = webdriver.Chrome()  #desired_capabilities=capabilities "No permitia correr las pruebas"
 
-    def test_set_route(self):
+    def test_1_set_route(self):
         self.driver.get(data.urban_routes_url)
+        time.sleep(5)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -76,6 +218,163 @@ class TestUrbanRoutes:
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
+    def test_2_comfort_button(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_text_perks == 'Blanket and handkerchiefs'
+
+    def test_3_phone_number(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+
+    def test_4_card_method_payment(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+        routes_page.set_card_method_payment()
+        payment_method_validation = routes_page.validated_payment_method()
+        assert payment_method_validation == 'Card'
+
+    def test_5_placed_comment_to_driver(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_validation_by_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_validation_by_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+        routes_page.set_card_method_payment()
+        payment_method_validation = routes_page.validated_payment_method()
+        assert payment_method_validation == 'Card'
+        routes_page.set_comment_to_driver()
+        comment_to_driver_validation_error = routes_page.validated_comment_to_driver()
+        assert comment_to_driver_validation_error == ''
+
+    def test_6_select_blankets_and_handkerchiefs(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_validation_by_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_validation_by_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+        routes_page.set_card_method_payment()
+        payment_method_validation = routes_page.validated_payment_method()
+        assert payment_method_validation == 'Card'
+        routes_page.set_comment_to_driver()
+        comment_to_driver_validation_error = routes_page.validated_comment_to_driver()
+        assert comment_to_driver_validation_error == ''
+        routes_page.set_request_blankets_and_handkerchiefs()
+
+    def test_7_select_ice_cream(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_validation_by_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_validation_by_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+        routes_page.set_card_method_payment()
+        payment_method_validation = routes_page.validated_payment_method()
+        assert payment_method_validation == 'Card'
+        routes_page.set_comment_to_driver()
+        comment_to_driver_validation_error = routes_page.validated_comment_to_driver()
+        assert comment_to_driver_validation_error == ''
+        routes_page.set_request_blankets_and_handkerchiefs()
+        routes_page.set_request_ice_cream()
+
+    def test_8_wait_for_call_a_taxi_active_button(self):
+        self.driver.get(data.urban_routes_url)
+        time.sleep(3)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.address_from
+        address_to = data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+        time.sleep(3)
+        routes_page.set_comfort_option()
+        time.sleep(3)
+        comfort_validation_by_text_perks = routes_page.validated_comfort_choice_with_perks()
+        assert comfort_validation_by_text_perks == 'Blanket and handkerchiefs'
+        routes_page.set_phone_number()
+        phone_number_validation = routes_page.validated_phone_number()
+        assert phone_number_validation == '+1 123 123 12 12'
+        routes_page.set_card_method_payment()
+        payment_method_validation = routes_page.validated_payment_method()
+        assert payment_method_validation == 'Card'
+        routes_page.set_comment_to_driver()
+        comment_to_driver_validation_error = routes_page.validated_comment_to_driver()
+        assert comment_to_driver_validation_error == ''
+        routes_page.set_request_blankets_and_handkerchiefs()
+        routes_page.set_request_ice_cream()
+        time.sleep(3)
+        description = routes_page.call_a_taxi_button_visibility()
+        assert description == 'Call a taxi\nThe route will run 1 kilometers and take 1 minutes.'
 
     @classmethod
     def teardown_class(cls):
